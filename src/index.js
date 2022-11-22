ee10be9eae70d685d0b07052105747db3759b403
Rz0R <vpa05@mail.ru>
2022-11-22 13:28:25 +1000

    feat: add self-checking

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import MainPresenter from './presenter/main-presenter';
+import { displaySelfChecking } from './self-checking';
 
 const root = document.querySelector('#root');
 
 const app = new MainPresenter(root);
 app.init();
+
+displaySelfChecking();
